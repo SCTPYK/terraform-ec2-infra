ae7b88b4bf7db4a409e7aa0a4b85936b65df942b
SCTPYK <xoxdx49@gmail.com>
2025-01-17 20:39:08 +0800

    terraform code for ec2

---
+++ main.tf
@@ -0,0 +1,70 @@
+resource "aws_instance" "public" {
+  ami                         = "ami-0454e52560c7f5c55" #Challenge, find the AMI ID of Amazon Linux 2 in us-east-1
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.yk_subnet.id  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+  associate_public_ip_address = true
+  key_name                    = "ykwong_ce9" #Change to your keyname, e.g. jazeel-key-pair
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+ 
+  tags = {
+    Name = "ykwong-ec2"    #Prefix your own name, e.g. jazeel-ec2
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "" #Security group name, e.g. jazeel-terraform-security-group
+  description = "Allow SSH inbound"
+  vpc_id      = aws_vpc.yk_vpc.id  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"  
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc" "yk_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "yk_vpc"
+  }
+}
+
+resource "aws_subnet" "yk_subnet" {
+  vpc_id = aws_vpc.yk_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "yk-public-subnet"
+  }
+}
+
+resource "aws_route_table" "yk-public" {
+  vpc_id = aws_vpc.yk_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.yk_vpc.id
+
+  tags = {
+    Name = "internet-gateway"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet-association" {
+  subnet_id = aws_subnet.yk_subnet.id
+  route_table_id = aws_route_table.yk-public.id
+}
